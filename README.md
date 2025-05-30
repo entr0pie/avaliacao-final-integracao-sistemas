40e9526ed674094020b60ac5f914156becd98b1d
CAIO HENRIQUE PORCEL <caio.porcel@cs.up.edu.br>
2025-05-30 20:56:55 -0300

    feat: adiciona integração com rabbitmq ao events-service

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 ```sh
 docker run -p 6379:6379 redis
 ```
+
+### RabbitMQ
+
+```sh
+docker run -p 5672:5672 -p 15672:15672 rabbitmq:management
+```
--- events-service/app.py
+++ events-service/app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, request, jsonify
 import redis
+import pika
+import threading
 
 app = Flask(__name__)
 redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
@@ -25,5 +27,20 @@ def get_events():
     
     return jsonify({"events": cached_events if cached_events else events})
 
+QUEUE = 'dispatch-messages'
+
+def receive_dispatch_alert(ch, method, properties, body):
+    print(f"Mensagem recebida do logistic-service: {body.decode()}")
+
+def consume():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE)
+    channel.basic_consume(queue=QUEUE, on_message_callback=receive_dispatch_alert, auto_ack=True)
+    channel.start_consuming()
+
 if __name__ == "__main__":
+    thread = threading.Thread(target=consume, daemon=True)
+    thread.start()
+
     app.run(debug=True)
